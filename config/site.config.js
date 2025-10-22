92ef95ee4ffa6c818e718893052780d94a47dc39
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-10-23 07:43:53 +0900

    ルート相対パス → 相対パス

--- config/site.config.js
+++ config/site.config.js
@@ -4,32 +4,23 @@ export const siteConfig = {
    */
 
   // パス設定
-  EJS_PATH: "./ejs/", // ルート相対パスで書くと、ズレる（システムルートの解釈）
-  IMAGE_PATH: "/assets/images/",
-  CSS_PATH: "/assets/sass/style.scss",
-  JS_PATH: "/assets/js/main.js",
-  // ROOT_PATH: "/",
-  FONT_PATH: "/fonts/NotoSansJP-VF.woff2",
+  ejsPath: "./ejs/", // ルート相対パスで書くと、ズレる（システムルートの解釈）
+  imagePath: "/assets/images/",
 
-  // アイコン・画像設定
-  FAVICON_PATH: "/vite.svg",
-  FAVICON_ICO: "/favicon.ico",
-  APPLE_TOUCH_ICON: "/apple-touch-icon.png",
-  OGP_IMAGE: "/ogp.png",
 
   /**
    * プロジェクト設定（案件固有のもの）
    */
   // サイト基本情報
   siteName: "静的サイト用ejsテンプレ",
-  DOMAIN: "https://example.com",
+  domain: "https://rea1i2e.net/",
   titleSeparator: " | ",
   headerExcludePages: ["privacy"], // ヘッダーから除外するページ
   pages: {
     top: {
       label: "トップ",
       root: "./",
-      path: "/",
+      path: "",
       title: "",
       description:
       "トップページの説明文です。サイトの概要や特徴を簡潔に記載してください。",
@@ -53,7 +44,7 @@ export const siteConfig = {
     privacy: {
       label: "プライバシーポリシー",
       root: "../",
-      path: "/privacy/",
+      path: "privacy/",
       title: "プライバシーポリシー",
       description:
       "プライバシーポリシーページです。個人情報の取り扱いについて説明しています。",
