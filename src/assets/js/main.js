998450791bca632b86161417a372e48b306c8f6e
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-12-06 09:46:45 +0900

    Organize imports in main.js for better structure and clarity, grouping by functionality

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,19 @@
-// import './_drawer.js';
-import './_slider.js';
-// import './_dialog.js';
+/* 共通 */
+import './_drawer.js';
+import './_initializeSmoothScroll.js';
 // import './_header.js';
+
+
+/* 機能ごと */
+import './_slider.js';
 import './_fadein.js';
 // import './_counter.js';
 // import './_tab.js';
+// import './_dialog.js';
 // import './_modal.js';
+import './_accordion.js';
+
+/* フォーム関連 */
 import './_option-color.js';
 import './_checkFormValidity.js';
 import './_flatpickr.js';
-import './_accordion.js';
