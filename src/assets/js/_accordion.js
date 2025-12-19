2f0b8b534ac2730b16b436eddd35c82914283988
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-12-19 13:09:13 +0900

    不要なコンソールログ削除

--- src/assets/js/_accordion.js
+++ src/assets/js/_accordion.js
@@ -3,8 +3,6 @@
  * @see https://ics.media/entry/220901/#javascript%E3%81%A7%E9%96%8B%E9%96%89%E3%82%A2%E3%83%8B%E3%83%A1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%82%92%E3%81%A4%E3%81%91%E3%82%8B%E6%96%B9%E6%B3%95%EF%BC%88%E5%85%A8%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC%E5%AF%BE%E5%BF%9C%EF%BC%89
  */
 
-console.log("accordion.js");
-
 /**
  * アニメーションの時間とイージング
  */
