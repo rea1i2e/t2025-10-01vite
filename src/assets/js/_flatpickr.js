e6b2a652868212d10c05b9c76f1d1330b3dd9574
Yoshiaki Numajiri <afmaar128@gmail.com>
2026-03-26 12:38:13 +0900

    キャッシュをクリアして.gitignoreの変更を反映

--- src/assets/js/_flatpickr.js
+++
@@ -1,9 +0,0 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import { Japanese } from "flatpickr/dist/l10n/ja.js";
-
-flatpickr("#js-flatpickr-date", {
-  locale: Japanese,
-  dateFormat: "Y-m-d",
-  disableMobile: true // モバイルのネイティブUIを無効化
-});
