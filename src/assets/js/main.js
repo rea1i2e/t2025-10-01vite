ddf32b5a1c9b7f019793a40a3449621d3a979a0d
Yoshiaki Numajiri <afmaar128@gmail.com>
2026-03-31 17:15:01 +0900

    demo-js整理

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,63 +1,88 @@
-/* 共通 */
+/**
+* デバッグ用
+*/
+
+// スクロール可能要素の調査
+// import './_debugScrollable.js';
+
+
+/**
+* 共通処理
+*/
+
+/* スムーススクロール */
 import './_initializeSmoothScroll.js';
+
+/* ドロワー */
 import './_drawer.js';
-import './_toggle.js';
-import './_theme-toggle.js'; // Demo用。案件時は削除すること
-
-// import './_header.js';
 
 
-/* ファーストビュー動画デモ */
-import './_demo-fv-video.js';
+
+/**
+ * 案件固有処理（必要に応じてdemoから移動して使用）
+ */
 
 /* スライダー */
-import './_splide-fade.js';
-import './_splide-loop.js';
-import './_splide-thumbnail.js';
-import './_splide-progress.js';
+import './demo/_splide-fade.js';
+import './demo/_splide-loop.js';
+import './demo/_splide-thumbnail.js';
+import './demo/_splide-progress.js';
 
-/* スクロールに応じた表示制御 */
-import './_fadein.js';
-import './_nav-current-section.js';
-import './_page-top.js';
-import './_fixed-menu.js';
-import './_counter.js';
-import './_tab.js';
-// import './_modal.js';
-import './_accordion.js';
-
-/* フォーム関連 */
-import './_option-color.js';
-import './_checkFormValidity.js';
-import './_flatpickr.js';
+/* アコーディオン・トグル */
+import './demo/_accordion.js';
+import './demo/_toggle.js'; // details, summaryを使わない場合
 
 /* モーダル */
 // import './_dialog-common.js'; // 各ファイルから読み込んでいるため、ここでの読み込みは不要
-import './_dialog-general.js';
-import './_dialog-youtube.js';
-import './_dialog-video.js';
+import './demo/_dialog-general.js';
+import './demo/_dialog-youtube.js'; // モーダルのyoutube動画再生
+import './demo/_dialog-video.js'; // モーダルのvideo要素動画再生
+import './demo/_modal.js'; // dialog要素を使わない場合
 
-/* メールアドレス保護 */
-import './_email-protection.js';
+/* タブ切り替え */
+import './demo/_tab.js';
+
+/* スクロールに応じた表示制御 */
+import './demo/_fadein.js';
+import './demo/_parallax.js'; // パララックス
+import './demo/_page-top.js';
+import './demo/_fixed-menu.js';
+import './demo/_nav-current-section.js';
+import './demo/_counter.js'; // カウントアップアニメーション
+import './demo/_header.js';
 
 /* スクロールヒント */
-import './_scroll-hint.js';
-
-/* テキストアンダーライン */
-import './_text-underline.js';
-
-/* パララックス */
-import './_parallax.js';
+import './demo/_scroll-hint.js';
 
 
+/* フォーム関連 */
+import './demo/_option-color.js';
+import './demo/_checkFormValidity.js';
+import './demo/_flatpickr.js';
 
 
-/* スクロール可能要素の調査（必要に応じてコメントアウトを外す） */
-// import './_debugScrollable.js';
+
+/* メールアドレス保護 */
+import './demo/_email-protection.js';
+
+
+/* ホバーエフェクト */
+import './demo/_text-underline.js'; // 左から右に下線
+
+
+
+
+/* ファーストビュー動画デモ */
+import './demo/_demo-fv-video.js';
 
 
 /* JSONPlaceholder API デモ */
 import './demo/_demo-api.js';
 
+
 /* 音声 ON/OFF dialog デモ */
 import './demo/_demo-sound.js';
+
+
+/* ダークモード・ライトモード切り替え */
+import './demo/_theme-toggle.js';
